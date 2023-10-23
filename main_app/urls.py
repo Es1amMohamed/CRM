728b51134263bfef9aa69086f439c0c9e7b43cb0
Eslam Mohamed <eslammohamedmetwally1@gmail.com>
2023-10-23 19:17:10 +0300

    add templates and login form

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import include, path
+from . import views
+
 
 app_name = "main_app"
 
 urlpatterns = [
-    
+    path("", views.home, name="home"),
+    path("login_user", views.login_user, name="login_user"),
+    path("logout_user", views.logout_user, name="logout_user"),
 ]
